ce9ed521a5e61e18d3db557c5ed020f1cbb6f9f6
Heera <helgg@daum.net>
2024-01-13 14:04:36 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+r = int(input())
+if r >= 90 and r <= 100:
+    print("A")
+elif r >= 80 and r <= 89:
+    print("B")
+elif r >= 70 and r <= 79:
+    print("C")
+elif r >= 60 and r <= 69:
+    print("D")
+else:
+    print("F")
