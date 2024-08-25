b2872ea3e02d46ec197bce4af1b30c03578f42c2
Heera <helgg@daum.net>
2024-08-25 11:27:26 +0900

    [Bronze I] Title: 세로읽기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -1,17 +1,22 @@
 user_list = []
 n = 5
 string = ""
+max_length = 0
 
 for i in range(n):
     user_input = input()
     row_list = []
+    length = len(user_input)
     
-    for j in range(len(user_input)):
+    for j in range(length):
         row_list.append(user_input[j])
+    
+    if max_length < length:
+        max_length = length
     
     user_list.append(row_list)
 
-for a in range(max(len(row) for row in user_list)): 
+for a in range(max_length): 
     for b in range(n):
         if a < len(user_list[b]):  
             string += user_list[b][a]
